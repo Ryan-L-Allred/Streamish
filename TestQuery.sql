fbb2eb343aca2ab87b4d67e70d2cf8ed5ee96a68
Ryan Allred <ryanallred91@gmail.com>
2023-01-20 13:20:35 -0600

    This is first push

---
+++ TestQuery.sql
@@ -0,0 +1,4 @@
+﻿SELECT up.Id , up.Name, up.Email, up.ImageUrl, up.DateCreated AS UserProfileDateCreated, 
+	   v.Id AS VideoId, v.Title, v.Description, v.Url, v.DateCreated AS VideoDateCreated, v.UserProfileId   
+FROM UserProfile up
+	JOIN Video v ON up.Id = v.UserProfileId;
