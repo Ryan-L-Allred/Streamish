1a48a45ca9746c9b65975699598d59afb8a5fc9f
Ryan Allred <ryanallred91@gmail.com>
2023-02-03 14:09:43 -0600

    Updating repo

---
+++ Streamish/client/src/components/Video.js
@@ -0,0 +1,28 @@
+import React from "react";
+import { Card, CardBody } from "reactstrap";
+import { Link } from "react-router-dom";
+
+const Video = ({ video }) => {
+  return (
+    <Card >
+      <p className="text-left px-2">Posted by: {video.userProfile.name}</p>
+      <CardBody>
+        <iframe className="video"
+          src={video.url}
+          title="YouTube video player"
+          frameBorder="0"
+          allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+          allowFullScreen />
+
+<Link to={`/videos/${video.id}`}>
+    <strong>{video.title}</strong>
+</Link>
+        <p>{video.description}</p>
+        <p>{video.comments.map((c) => (c.message))}</p>
+        
+      </CardBody>
+    </Card>
+  );
+};
+
+export default Video;
